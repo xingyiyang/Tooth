cd8310481c2bb55694478a0b8c8558b484145b54
xingyiyang <532956941@qq.com>
2017-08-27 15:28:54 +0800

    版本1，完成数据库查询，查询列表展示

--- app/src/main/java/com/scu/xing/tooth/ListActivity.java
+++ app/src/main/java/com/scu/xing/tooth/ListActivity.java
@@ -1,24 +1,32 @@
 package com.scu.xing.tooth;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AlertDialog.Builder;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.scu.xing.tooth.dbhelper.DBHelper;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
+ * 查询结果展示页面
  * Created by xing on 2017/8/14.
  */
 
@@ -28,14 +36,24 @@ public class ListActivity extends AppCompatActivity{
     private TextView textView_jiancha;
     private ListView listView;
     private Toolbar toolbar;
-    private String[] data={"apple","banana","orange"};
+    private ImageView imagenoresult;
+
+    //使用自定义的DBHelper对数据操作
+    DBHelper helper = new DBHelper(ListActivity.this);
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listview);
 
-        toolbar = (Toolbar) findViewById(R.id.list_tool_bar);
+        //初始化控件
+        initWidget();
+
+        //只读，不能修改数据库
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        //设置标题栏
         toolbar.setTitle("查询结果");
         setSupportActionBar(toolbar);
 
@@ -45,13 +63,120 @@ public class ListActivity extends AppCompatActivity{
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        //获取第一个页面参数的值，把参数的值展示出来
-        initTextView();
+        //查询所有数据
+        //queryAll();
 
-        //使用自定义的DBHelper对数据操作
-        DBHelper helper = new DBHelper(ListActivity.this);
-        SQLiteDatabase db = helper.getWritableDatabase();
+        //把第一个页面传过来的值拼接成字符串
+        String param= initTextView();
+        final Map<String,String> result=queryBykeyWord(db,param);
+        //如果map是空，说明没有该症状，显示查无此症，否则展示查询列表
+        if(result.isEmpty()){
+            listView.setVisibility(View.GONE);
+            imagenoresult.setVisibility(View.VISIBLE);
 
+        }else{
+            imagenoresult.setVisibility(View.GONE);
+            listView.setVisibility(View.VISIBLE);
+            //设置listview的数据
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(ListActivity.this,android.R.layout.simple_list_item_1,new ArrayList<>(result.keySet()));
+            listView.setAdapter(adapter);
+
+            //监听listview列表中的item
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    String title = listView.getItemAtPosition(i).toString();
+                    //Toast.makeText(ListActivity.this,str,Toast.LENGTH_SHORT).show();
+                    showDialog(title,result);
+                }
+            });
+        }
+
+
+    }
+
+    private void initWidget(){
+        textView_tengtong = (TextView)findViewById(R.id.textView_tengtong);
+        textView_jiancha = (TextView)findViewById(R.id.textView_jiancha);
+        listView = (ListView)findViewById(R.id.list_view);
+        imagenoresult = (ImageView)findViewById(R.id.imagenoresult);
+        toolbar = (Toolbar) findViewById(R.id.list_tool_bar);
+    }
+
+    /**
+     * 获取上一个页面查询的参数
+     */
+    private String initTextView(){
+
+        Bundle bundle = getIntent().getExtras();
+
+        String tengtong1 = bundle.getString("tengtongfirst");
+        String tengtong2 = bundle.getString("tengtongsecond");
+        String tengtong11 = bundle.getString("tengtongfirst2");
+        String tengtong12 = bundle.getString("tengtongsecond2");
+
+        String jiancha = bundle.getString("jiancha");
+        String waixing = bundle.getString("waixing");
+        String qita = bundle.getString("qita");
+
+        String jiancha2 = bundle.getString("jiancha2");
+        String waixing2 = bundle.getString("waixing2");
+        String qita2 = bundle.getString("qita2");
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        //第一组疼痛
+        textView_tengtong.setText("疼痛类型：");
+        if(tengtong1.equals("激发痛+")){
+            textView_tengtong.setText(textView_tengtong.getText()+tengtong1+" / "+tengtong2);
+            stringBuilder.append("%"+tengtong1+"%");
+            stringBuilder.append("%"+tengtong2+"%");
+        }else {
+            textView_tengtong.setText(textView_tengtong.getText()+tengtong1);
+            stringBuilder.append("%"+tengtong1+"%");
+        }
+
+        //第二组疼痛
+        if(!tengtong11.equals("选择疼痛类型")){
+            if(tengtong11.equals("激发痛+")){
+                textView_tengtong.setText(textView_tengtong.getText()+" / "+tengtong11+" / "+tengtong12);
+                stringBuilder.append("%"+tengtong11+"%");
+                stringBuilder.append("%"+tengtong12+"%");
+            }else{
+                textView_tengtong.setText(textView_tengtong.getText()+" / "+tengtong11);
+                stringBuilder.append("%"+tengtong11+"%");
+            }
+
+        }
+
+        //第一组检查
+        textView_jiancha.setText("检查类型：");
+        if(jiancha.equals("外形")){
+            textView_jiancha.setText(textView_jiancha.getText()+waixing);
+            stringBuilder.append("%"+waixing+"%");
+        }else{
+            textView_jiancha.setText(textView_jiancha.getText()+qita);
+            stringBuilder.append("%"+qita+"%");
+        }
+
+        //第二组检查
+        if(jiancha2.equals("外形")){
+            textView_jiancha.setText(textView_jiancha.getText()+" / "+waixing2);
+            stringBuilder.append("%"+waixing2+"%");
+        }
+        if (jiancha2.equals("其他")){
+            textView_jiancha.setText(textView_jiancha.getText()+" / "+qita2);
+            stringBuilder.append("%"+qita2+"%");
+        }
+
+        return stringBuilder.toString().trim();
+
+    }
+
+    /**
+     * 使用数据库查询所有列表
+     */
+    public void queryAll(SQLiteDatabase db){
         //查询数据库中的数据
         Cursor c = db.rawQuery("select * from tooth_table",null);
         //遍历数据库中的数据
@@ -65,60 +190,71 @@ public class ListActivity extends AppCompatActivity{
             c.close();
         }
         db.close();
-
-        //设置listview的数据
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ListActivity.this,android.R.layout.simple_list_item_1,data);
-        listView = (ListView)findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
-
-        //监听listview列表中的item
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String str = listView.getItemAtPosition(i).toString();
-                Toast.makeText(ListActivity.this,str,Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     /**
-     * 获取上一个页面查询的参数
+     * 根据关键字查询数据列表
      */
-    private void initTextView(){
-        Bundle bundle = getIntent().getExtras();
-        String tengtong1 = bundle.getString("tengtongfirst");
-        String tengtong2 = bundle.getString("tengtongsecond");
-        String jiancha = bundle.getString("jiancha");
-        String waixing = bundle.getString("waixing");
-        String qita = bundle.getString("qita");
-        String tengtongkeyword = bundle.getString("tengtongkeyword");
-        String jianchakeyword = bundle.getString("jianchakeyword");
+    /*public ArrayList<String> queryBykeyWord(SQLiteDatabase db,String param){
 
-        textView_tengtong = (TextView)findViewById(R.id.textView_tengtong);
-        textView_jiancha = (TextView)findViewById(R.id.textView_jiancha);
+        ArrayList<String> result = new ArrayList<String>();
+        Cursor c = db.query("tooth_table",null,"key like ?",new String[]{param},null,null,null);
+        if(c!=null){
+            while (c.moveToNext()){
+                result.add(c.getString(c.getColumnIndex("name")));
+            }
+            c.close();
+        }else{
+            Toast.makeText(ListActivity.this,"查无此症",Toast.LENGTH_SHORT).show();
+        }
+        db.close();
+        return result;
+    }*/
 
-        textView_tengtong.setText("疼痛类型：");
-        if(tengtong1!=null){
-            textView_tengtong.setText(textView_tengtong.getText()+tengtong1);
-        }
-        if(tengtong1.equals("激发痛+") && tengtong2!=null){
-            textView_tengtong.setText(textView_tengtong.getText()+" / "+tengtong2);
-        }
-        if(tengtongkeyword!=null){
-            textView_tengtong.setText(textView_tengtong.getText()+" / "+tengtongkeyword);
-        }
+    public Map<String,String> queryBykeyWord(SQLiteDatabase db,String param){
 
-        textView_jiancha.setText("检查类型：");
-        if(jiancha.equals("外形")){
-            textView_jiancha.setText(textView_jiancha.getText()+jiancha+" / "+waixing);
+        Map<String,String> result = new LinkedHashMap<String,String>();
+        Cursor c = db.query("tooth_table",null,"key like ?",new String[]{param},null,null,null);
+        if(c!=null){
+            while (c.moveToNext()){
+                result.put(c.getString(c.getColumnIndex("name")),c.getString(c.getColumnIndex("diagnosis")));
+            }
+            c.close();
         }
-        if(jiancha.equals("其他")){
-            textView_jiancha.setText(textView_jiancha.getText()+jiancha+" / "+qita);
-        }
-        if(jianchakeyword!=null){
-            textView_jiancha.setText(textView_jiancha.getText()+" / "+jianchakeyword);
-        }
+        db.close();
+        return result;
+    }
 
+    /**
+     * 点击item弹出dialog
+     */
+    public void showDialog(String title,Map<String,String> map){
+
+        String diagnosis = map.get(title);
+
+        Builder builder = new Builder(this);
+        builder.setTitle(title);
+        builder.setIcon(R.mipmap.tooth);
+        builder.setMessage(diagnosis);
+        builder.setPositiveButton("确定", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        //设置确定按钮的字体颜色为蓝色
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.BLUE);
+
+//        try {
+//            Field mAlert = AlertDialog.class.getDeclaredField("mAlert");
+//            mAlert.setAccessible(true);
+//            Object mAlertController = mAlert.get(dialog);
+//            Field mMessage = mAlertController.getClass().getDeclaredField("mMessageView");
+//            mMessage.setAccessible(true);
+//            TextView mMessageView = (TextView) mMessage.get(mAlertController);
+//            mMessageView.setTextColor(Color.BLUE);
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        } catch (NoSuchFieldException e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
--- app/src/main/java/com/scu/xing/tooth/MainActivity.java
+++ app/src/main/java/com/scu/xing/tooth/MainActivity.java
@@ -9,22 +9,34 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * 主页面，用于查询的输入
+ * Created by xing on 2017/8/14.
+ */
 public class MainActivity extends AppCompatActivity {
 
-    Spinner sp_tengtong1;  //疼痛第一级
-    Spinner sp_tengtong2;  //疼痛第二级
-    Spinner sp_tt_blank;   //疼痛空列表
-    EditText editText_tengtong;  //疼痛的关键字
-    Spinner sp_jiancha;          //检查第一级
-    Spinner sp_jianchawaixing;   //检查第二级 外形
-    Spinner sp_jianchaqita;      //检查第二级 其他
-    Spinner sp_jc_blank;         //检查空列表
-    EditText editText_jiancha;   //检查的关键字
+    Spinner sp_tengtong1;  //第一个疼痛第一级
+    Spinner sp_tengtong2;  //第一个疼痛第二级
+    Spinner sp_tt_blank;   //第一个疼痛空列表
+
+    Spinner sp_tengtong11;  //第二个疼痛第一级
+    Spinner sp_tengtong12;  //第二个疼痛第二级
+    Spinner sp_tt_blank2;   //第二个疼痛空列表
+
+
+    Spinner sp_jiancha;          //第一个检查第一级
+    Spinner sp_jianchawaixing;   //第一个检查第二级 外形
+    Spinner sp_jianchaqita;      //第一个检查第二级 其他
+    Spinner sp_jc_blank;         //第一个检查空列表
+
+    Spinner sp_jiancha2;          //第二个检查第一级
+    Spinner sp_jianchawaixing2;   //第二个检查第二级 外形
+    Spinner sp_jianchaqita2;      //第二个检查第二级 其他
+    Spinner sp_jc_blank2;         //第二个检查空列表
+
     Bundle bundle;        //把选中的参数传到第二页面
 
     @Override
@@ -39,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.main_tool_bar);
         setSupportActionBar(toolbar);
 
+        //第一组疼痛开始
         //监听疼痛第一级列表
         sp_tengtong1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -80,7 +93,54 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        //第一组疼痛结束
 
+        //第二组疼痛开始
+        //监听疼痛第一级列表
+        sp_tengtong11.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //如果选择的是“激发痛+”，则展开第二级列表，否则隐藏第二级列表
+                if(i==1){
+                    sp_tengtong12.setVisibility(View.VISIBLE);
+                    sp_tt_blank2.setVisibility(View.GONE);
+                }else{
+                    sp_tengtong12.setVisibility(View.GONE);
+                    sp_tt_blank2.setVisibility(View.VISIBLE);
+                }
+
+                //获取spinner中选中的字符串
+                String tengtong2 = (String)sp_tengtong11.getSelectedItem();
+                //把字符串保存到bundle中，相当于保存在map里面
+                bundle.putString("tengtongfirst2",tengtong2);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        //监听疼痛第二级列表，"激发痛+"的第二级列表
+        sp_tengtong12.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String tengtong12 = (String)sp_tengtong12.getSelectedItem();
+                bundle.putString("tengtongsecond2",tengtong12);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        //第二组疼痛结束
+
+
+        //第一组检查开始
         //监听检查第一级列表
         sp_jiancha.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -125,8 +185,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //初始化检查第二级列表，其他，监听列表
-
+        //监听检查第二级列表，其他
         sp_jianchaqita.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -140,6 +199,68 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        //第一组检查结束
+
+        //第二组检查开始
+        //监听检查第一级列表
+        sp_jiancha2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if(i==1){
+                    sp_jianchawaixing2.setVisibility(View.VISIBLE);
+                    sp_jianchaqita2.setVisibility(View.GONE);
+                    sp_jc_blank2.setVisibility(View.GONE);
+                }else if(i == 2){
+                    sp_jianchaqita2.setVisibility(View.VISIBLE);
+                    sp_jianchawaixing2.setVisibility(View.GONE);
+                    sp_jc_blank2.setVisibility(View.GONE);
+                }else{
+                    sp_jc_blank2.setVisibility(View.VISIBLE);
+                    sp_jianchawaixing2.setVisibility(View.GONE);
+                    sp_jianchaqita2.setVisibility(View.GONE);
+                }
+
+                String str_jiancha2 = (String)sp_jiancha2.getSelectedItem();
+                bundle.putString("jiancha2",str_jiancha2);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        //监听检查第二级列表，外形
+        sp_jianchawaixing2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String str_waixing2 = (String)sp_jianchawaixing2.getSelectedItem();
+                bundle.putString("waixing2",str_waixing2);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        //监听检查第二级列表，其他
+        sp_jianchaqita2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String str_qita2 = (String)sp_jianchaqita2.getSelectedItem();
+                bundle.putString("qita2",str_qita2);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        //第二组检查结束
 
         //查询按钮监听
         findViewById(R.id.button_chaxuan).setOnClickListener(new View.OnClickListener() {
@@ -147,9 +268,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(sp_tengtong1.getSelectedItemPosition()!=0){
                     if(sp_jiancha.getSelectedItemPosition()!=0){
-                        getKeyword();
-                        Toast.makeText(MainActivity.this,"查询",Toast.LENGTH_SHORT).show();
+
                         Intent intent = new Intent(MainActivity.this,ListActivity.class);
+                        //把参数传递到第二个页面
                         intent.putExtras(bundle);
                         startActivity(intent);
                     }else{
@@ -163,40 +284,50 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * 设置顶部的标题栏
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
+    /**
+     * 监听重置按钮
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.reset) {
             Toast.makeText(MainActivity.this,"重置",Toast.LENGTH_SHORT).show();
             resetAll();
-
         }
         return super.onOptionsItemSelected(item);
     }
 
-    //重置选项，隐藏spinner，关闭toggleButton
+    /**
+     * 重置按钮，下拉列表重置
+     */
     public void resetAll(){
 
+        //疼痛重置
         sp_tengtong1.setSelection(0);
         sp_tengtong2.setSelection(0);
+        sp_tengtong11.setSelection(0);
+        sp_tengtong12.setSelection(0);
+
+        //检查重置
         sp_jiancha.setSelection(0);
         sp_jianchawaixing.setSelection(0);
         sp_jianchaqita.setSelection(0);
-    }
+        sp_jiancha2.setSelection(0);
+        sp_jianchawaixing2.setSelection(0);
+        sp_jianchaqita2.setSelection(0);
 
-    //获取输入的关键字
-    public void getKeyword(){
-        if(editText_tengtong.getText()!=null){
-            bundle.putString("tengtongkeyword",editText_tengtong.getText().toString());
-        }
-        if(editText_jiancha.getText()!=null){
-            bundle.putString("jianchakeyword",editText_jiancha.getText().toString());
-        }
     }
 
     /**
@@ -204,15 +335,28 @@ public class MainActivity extends AppCompatActivity {
      */
     public void initWidget(){
 
+        //第一组疼痛
         sp_tengtong1 = (Spinner)findViewById(R.id.spinner_tengtong1);  //疼痛第一级
         sp_tengtong2 = (Spinner)findViewById(R.id.spinner_tengtong2);  //疼痛第二级
         sp_tt_blank = (Spinner)findViewById(R.id.spinner_tt_blank);    //疼痛空
-        editText_tengtong = (EditText)findViewById(R.id.edt_tengtong); //疼痛关键字
+
+        //第二组疼痛
+        sp_tengtong11 = (Spinner)findViewById(R.id.spinner_tengtong11);  //疼痛第一级
+        sp_tengtong12 = (Spinner)findViewById(R.id.spinner_tengtong12);  //疼痛第二级
+        sp_tt_blank2 = (Spinner)findViewById(R.id.spinner_tt_blank2);    //疼痛空
+
+        //第一组检查
         sp_jiancha = (Spinner)findViewById(R.id.spinner_jiancha);      //检查第一级
         sp_jianchawaixing = (Spinner)findViewById(R.id.spinner_jianchawaixing);  //检查第二级 外形
         sp_jianchaqita = (Spinner)findViewById(R.id.spinner_jianchaqita);        //检查第二级 其他
         sp_jc_blank = (Spinner)findViewById(R.id.spinner_jc_blank);    //检查空列表
-        editText_jiancha = (EditText)findViewById(R.id.edt_jiancha);   //检查关键字
+
+        //第二组检查
+        sp_jiancha2 = (Spinner)findViewById(R.id.spinner_jiancha2);      //检查第一级
+        sp_jianchawaixing2 = (Spinner)findViewById(R.id.spinner_jianchawaixing2);  //检查第二级 外形
+        sp_jianchaqita2 = (Spinner)findViewById(R.id.spinner_jianchaqita2);        //检查第二级 其他
+        sp_jc_blank2 = (Spinner)findViewById(R.id.spinner_jc_blank2);    //检查空列表
+
         bundle = new Bundle();  //键值对bundle
     }
 
